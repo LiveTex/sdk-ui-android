1b231d2992215b573d48e6f9216a138111b366ae
Maxim Smirnov <maxim.smirnov@mercurydevelopment.com>
2023-04-20 23:12:34 +0400

    Made image zoomable and full size in ImageActivity

--- demo-lib/src/main/java/ru/livetex/sdkui/chat/image/ImageActivity.java
+++ demo-lib/src/main/java/ru/livetex/sdkui/chat/image/ImageActivity.java
@@ -1,7 +1,6 @@
 package ru.livetex.sdkui.chat.image;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
@@ -9,10 +8,8 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -31,7 +28,6 @@ public class ImageActivity extends AppCompatActivity {
 	public static final String EXTRA_URL = "EXTRA_URL";
 
 	private ImageView imageView;
-	private GestureDetector gestureDetector;
 
 	private final ActivityResultLauncher<String> requestPermissionLauncher =
 			registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -44,8 +40,6 @@ public class ImageActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_image);
-
-		gestureDetector = new GestureDetector(this, new MyGestureDetector(this));
 
 		MaterialToolbar toolbar = findViewById(R.id.toolbar);
 
@@ -63,35 +57,8 @@ public class ImageActivity extends AppCompatActivity {
 				.load(url)
 				.placeholder(R.drawable.placeholder)
 				.error(R.drawable.placeholder)
-				.centerCrop()
 				.dontAnimate()
 				.into(imageView);
-	}
-
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev) {
-		// TouchEvent dispatcher.
-		if (gestureDetector != null) {
-			if (gestureDetector.onTouchEvent(ev))
-			// If the gestureDetector handles the event, a swipe has been
-			// executed and no more needs to be done.
-			{
-				return true;
-			}
-		}
-
-		return super.dispatchTouchEvent(ev);
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		return gestureDetector.onTouchEvent(event);
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		gestureDetector = null;
 	}
 
 	@Override
@@ -127,44 +94,6 @@ public class ImageActivity extends AppCompatActivity {
 		DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
 		dm.enqueue(r);
 		Toast.makeText(this, "Сохранено", Toast.LENGTH_SHORT).show();
-	}
-
-	private static class MyGestureDetector extends GestureDetector.SimpleOnGestureListener {
-		private static final int SWIPE_MIN_DISTANCE = 150;
-		private static final int SWIPE_MAX_OFF_PATH = 300;
-		private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-
-		private final Activity activity;
-
-		private MyGestureDetector(Activity activity) {this.activity = activity;}
-
-		@Override
-		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
-			// Check movement along the Y-axis. If it exceeds SWIPE_MAX_OFF_PATH,
-			// then dismiss the swipe.
-			if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
-				return false;
-			}
-
-			// from left to right
-			if (e2.getX() > e1.getX()) {
-				if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-					activity.finish();
-					return true;
-				}
-			}
-
-			// from right to left
-			if (e1.getX() > e2.getX()) {
-				if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-					activity.finish();
-					return true;
-				}
-			}
-
-			return false;
-		}
 	}
 
 	public static void start(Context context, String url) {
